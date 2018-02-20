b6cca2e86247e82d665a77d132861a19edfdbc8c
davinryan <ryandavin@gmail.com>
2018-02-20 22:33:53 +1300

    More updates to comment out code for as needed basis

--- src/components/examples/LoadingSpinner/LoadingSpinner.js
+++ src/components/examples/LoadingSpinner/LoadingSpinner.js
@@ -4,37 +4,37 @@ import WithLoadingSpinnerRecompose from "../../../containers/enhancers/recompose
 import WithLoadingSpinner from "../../../containers/enhancers/without-recompose/withLoadingSpinner/WithLoadingSpinner";
 import "./LoadingSpinner.css";
 
-const fetchData = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(
-      () =>
-        resolve({
-          name: "Davin Ryan",
-          interests: "Javascript, React and Recompose",
-          gender: "male",
-          height: "183cm"
-        }),
-      2000
-    );
-  });
-};
+// const fetchData = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(
+//       () =>
+//         resolve({
+//           name: "Davin Ryan",
+//           interests: "Javascript, React and Recompose",
+//           gender: "male",
+//           height: "183cm"
+//         }),
+//       2000
+//     );
+//   });
+// };
 
-const MyDetails = ({ name, interests, gender, height }) => (
-  <div className="User">
-    <h3>{name}</h3>
-    <ul style={{ listStyle: "none" }}>
-      <li>Interests: {interests}</li>
-      <li>Gender: {gender}</li>
-      <li>Height: {height}</li>
-    </ul>
-  </div>
-);
+// const MyDetails = ({ name, interests, gender, height }) => (
+//   <div className="User">
+//     <h3>{name}</h3>
+//     <ul style={{ listStyle: "none" }}>
+//       <li>Interests: {interests}</li>
+//       <li>Gender: {gender}</li>
+//       <li>Height: {height}</li>
+//     </ul>
+//   </div>
+// );
 
 const LoadingSpinnerExample = () => {
-  const MyDetailsWithoutRecompose = WithLoadingSpinner(fetchData)(MyDetails);
-  const MyDetailsWithRecompose = WithLoadingSpinnerRecompose(fetchData)(
-    MyDetails
-  );
+  // const MyDetailsWithoutRecompose = WithLoadingSpinner(fetchData)(MyDetails);
+  // const MyDetailsWithRecompose = WithLoadingSpinnerRecompose(fetchData)(
+  //   MyDetails
+  // );
 
   return (
     <div>
@@ -42,12 +42,12 @@ const LoadingSpinnerExample = () => {
 
       <h3>Without Recompose</h3>
       <div className="loadingSpinner-exampleBox">
-        <MyDetailsWithoutRecompose />
+        {/*<MyDetailsWithoutRecompose />*/}
       </div>
 
       <h3>With Recompose</h3>
       <div className="loadingSpinner-exampleBox">
-        <MyDetailsWithRecompose />
+        {/*<MyDetailsWithRecompose />*/}
       </div>
     </div>
   );
