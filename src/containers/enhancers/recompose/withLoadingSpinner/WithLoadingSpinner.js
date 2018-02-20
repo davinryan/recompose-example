da5a59c09af8a74491d10f4f7d77320d1e9e829b
davinryan <ryandavin@gmail.com>
2018-02-20 22:28:36 +1300

    Commented out lines for demo

--- src/containers/enhancers/recompose/withLoadingSpinner/WithLoadingSpinner.js
+++ src/containers/enhancers/recompose/withLoadingSpinner/WithLoadingSpinner.js
@@ -3,35 +3,36 @@ import { compose, lifecycle, branch, renderComponent } from 'recompose';
 import './WithLoadingSpinner.css';
 
 export default fetchData => WrappedComponent => {
-  const Spinner = () =>
-  <div className='spinnerPage'>
-    <div id='cssloadPgloading'>
-      <div className='cssloadLoadingwrap'>
-        <ul className='cssloadBokeh'>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
-      </div>
-    </div>
-  </div>
+  // const Spinner = () =>
+  // <div className='spinnerPage'>
+  //   <div id='cssloadPgloading'>
+  //     <div className='cssloadLoadingwrap'>
+  //       <ul className='cssloadBokeh'>
+  //         <li></li>
+  //         <li></li>
+  //         <li></li>
+  //         <li></li>
+  //       </ul>
+  //     </div>
+  //   </div>
+  // </div>
 
-  const withData = lifecycle({
-    state: { loading: true },
-    componentDidMount() {
-      fetchData().then((data) =>
-        this.setState({ loading: false, ...data }));
-    }
-  });
+  // const withData = lifecycle({
+  //   state: { loading: true },
+  //   componentDidMount() {
+  //     fetchData().then((data) =>
+  //       this.setState({ loading: false, ...data }));
+  //   }
+  // });
 
-  const withSpinnerWhileLoading = branch(
-    ({ loading }) => loading,
-    renderComponent(Spinner)
-  );
+  // const withSpinnerWhileLoading = branch(
+  //   ({ loading }) => loading,
+  //   renderComponent(Spinner)
+  // );
 
-  return compose(
-    withData,
-    withSpinnerWhileLoading
-  )(WrappedComponent);
+  // return compose(
+  //   withData,
+  //   withSpinnerWhileLoading
+  // )(WrappedComponent);
+  return WrappedComponent;
 }
--- src/containers/enhancers/without-recompose/withLoadingSpinner/WithLoadingSpinner.js
+++ src/containers/enhancers/without-recompose/withLoadingSpinner/WithLoadingSpinner.js
@@ -2,39 +2,42 @@ import React from 'react';
 import './WithLoadingSpinner.css';
 
 export default fetchData => WrappedComponent => {
-  const Spinner = () =>
-    <div className='spinner'>
-      <div id='cssloadPgloading'>
-        <div className='cssloadLoadingwrap'>
-          <ul className='cssloadBokeh'>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
-        </div>
-      </div>
-    </div>;
+  // const Spinner = () =>
+  //   <div className='spinner'>
+  //     <div id='cssloadPgloading'>
+  //       <div className='cssloadLoadingwrap'>
+  //         <ul className='cssloadBokeh'>
+  //           <li></li>
+  //           <li></li>
+  //           <li></li>
+  //           <li></li>
+  //         </ul>
+  //       </div>
+  //     </div>
+  //   </div>;
 
   return class extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {loading: true};
-    }
+    // constructor(props) {
+    //   super(props);
+    //   this.state = {loading: true};
+    // }
 
-    componentDidMount() {
-      fetchData().then((data) =>
-        this.setState({ loading: false, ...data }));
-    }
+    // componentDidMount() {
+    //   fetchData().then((data) =>
+    //     this.setState({ loading: false, ...data }));
+    // }
 
     render() {
-      const {loading, ...data} = this.state;
+      // const {loading, ...data} = this.state;
+      // return (
+      //   <div>
+      //     {loading 
+      //     ? (<Spinner />) 
+      //     : (<WrappedComponent {...data} />)}
+      //   </div>
+      // );
       return (
-        <div>
-          {loading 
-          ? (<Spinner />) 
-          : (<WrappedComponent {...data} />)}
-        </div>
+        <WrappedComponent/>
       );
     }
   }
